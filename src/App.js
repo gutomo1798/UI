a71ebf915e2363b22301c57d77ec3722f99f8600
momo_toms <gutomojr@gmail.com>
2019-12-13 19:14:01 +0700

    ui

--- src/App.js
+++ src/App.js
@@ -1,26 +1,42 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
+import Box from '@material-ui/core/Box';
+import Textfield from '@material-ui/core/TextField';
+import Button from '@material-ui/core/Button';
+
 function App() {
+    this.state = {
+      nama : 'Gutomo'
+    };
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+        <div className="app">
+          <Box Component="span" display="block">
+            <Textfield
+              id="standart-with-placeholder"
+              label="Nama lengkap"
+              placeholder="Masukan nama"
+              margin="normal"
+              value="{this.state.nama}"
+              onChange={this.handleNama}
+              />
+            </Box>
+              
+            <Box>
+              <Button variant="contained" color="primary" type="submit">
+                Ganti Nama
+              </Button>
+            </Box>
+          <form>
+          <nama name={this.state.name}/>
+          </form>
+        </div>
+   );
+   
 }
 
-export default App;
+
+
+export default App
